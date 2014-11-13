4c583156556d2ffbae62bed56f9222b85520069e
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2014-11-13 12:44:20 +0900

    first commit

---
+++ html/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+
+
+
+  <style type="text/css">
+    #contents{
+      width:880px;
+      margin:0 auto;
+    }
+
+    #slide-wrap{
+      overflow:hidden;
+      height:60px;
+    }
+
+    #slide{
+      width:10000px;
+      padding: 0;
+    }
+
+    #slide > li {
+      height:60px;
+      float:left;
+      list-style:none;
+      background-color: #ccc;
+      margin-right:10px;
+    }
+    #prev{
+      float:left;
+    }
+    #next{
+      float:right;
+    }
+    #guide{
+      position:absolute;
+      width:90px;
+      height:60px;
+      background-color:red;
+      opacity:0.4;
+      left: 50%;
+      margin-left: -45px;
+    }
+  </style>
+<!-- めんどうくさかったのでCDNまんまで -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="lodash.underscore.min.js"></script>
+
+</head>
+<body>
+<div id="contents">
+
+  <div id="slide-wrap">
+    <ul id="slide">
+    <?php
+
+    $width_list = array(90,100,110,120,130);
+
+    for ($i = 0; $i < 100; $i++){
+
+      $width = $width_list[array_rand($width_list)];
+      echo "<li style='width:{$width}px'>$i:{$width}px</li>\n";
+    }
+
+    ?>
+    </ul>
+    <div id="guide">&nbsp;</div>
+
+  </div>
+
+  <div id="control">
+    <a id="prev" href="#TODO"><<</a><a id="next" href="#TODO">>></a>
+  </div>
+
+</div>
+
+<script src="slide.js"></script>
+<script type="text/javascript">
+
+
+</script>
+</body>
+</html>
---
+++ html/slide.js
@@ -0,0 +1,119 @@
+(function() {
+
+  var _config = {
+    slide:{
+      duration:200
+    }
+  }
+
+  var Slider = function(){
+    console.time('init');
+    this.init();
+    console.timeEnd('init');
+
+  }
+  Slider.prototype = {
+
+    init:function(){
+      var self = this;
+
+      this.$slide = $("#slide")
+      this.$slide_lists = this.$slide.find("li");
+
+      this.setSlideWidth();
+
+      this.current_index = this.getSlideIndex();
+
+      this.slideLists();
+
+      $("#prev").on("click",function(){
+        self.current_index--;
+
+        //とぅーどうー
+        if (self.current_index < 0)
+          self.current_index = 99;
+
+        self.slideLists();
+      })
+      $("#next").on("click",function(){
+        self.current_index++;
+
+        //とぅーどうー
+        if (self.current_index >= 100)
+          self.current_index = 0;
+
+        self.slideLists();
+      })
+    },
+    setSlideWidth:function(){
+
+      var width_all = 0;
+      var self = this;
+      var margin = this.getSlideListMargin();
+      this.slide_wrapper_width = $("#slide-wrap").width();
+
+      this.slide_lists = {
+        position:[],
+        width:[]
+      };
+
+      this.$slide_lists.each(function(i){
+
+        var $me = $(this)
+        var width = $me.width();
+        self.slide_lists.width[i] = width + margin;
+
+        width_all += self.slide_lists.width[i]
+        self.slide_lists.position[i] = width_all - margin - (width / 2);
+      })
+
+      this.slide_width = width_all;
+      this.$slide.width(width_all);
+    },
+
+    getSlideListMargin:function(){
+      return getMargin(this.$slide_lists,"margin-right");
+    },
+
+    slideLists:function(){
+      var index = this.current_index;
+      var position = this.slide_lists.position[index] * -1 + (this.slide_wrapper_width / 2);
+
+      this.animateSlideLists(position);
+    },
+    animateSlideLists:function(position){
+
+      var df = new $.Deferred();
+      var self = this;
+      var duration = _config.slide.duration;
+
+      this.is_animating = true;
+
+      this.$slide.stop(true,false).animate(
+        {"margin-left":position},
+        duration,
+        function(){
+          self.is_animating = false;
+          df.resolve();
+      });
+
+      return df.promise();
+
+    },
+
+    getSlideIndex:function(){
+      //TODOTODO
+      return 50;
+    }
+
+  }
+
+  function getMargin($target,prop){
+    return parseInt($target.css(prop));
+  }
+
+  var slider = new Slider();
+
+
+
+})()
