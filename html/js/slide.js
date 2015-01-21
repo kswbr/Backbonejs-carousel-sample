4fdfc644a66dda35ccf6e0387baeb91095171c20
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2015-01-21 17:22:01 +0900

    animation処理を拡張

--- html/js/slide.js
+++ html/js/slide.js
@@ -1,81 +1,8 @@
-// Production steps of ECMA-262, Edition 5, 15.4.4.19
-// Reference: http://es5.github.com/#x15.4.4.19
-if (!Array.prototype.map) {
-  Array.prototype.map = function(callback, thisArg) {
-
-    var T, A, k;
-
-    if (this == null) {
-      throw new TypeError(" this is null or not defined");
-    }
-
-    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-    var O = Object(this);
-
-    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-    // 3. Let len be ToUint32(lenValue).
-    var len = O.length >>> 0;
-
-    // 4. If IsCallable(callback) is false, throw a TypeError exception.
-    // See: http://es5.github.com/#x9.11
-    if ({}.toString.call(callback) != "[object Function]") {
-      throw new TypeError(callback + " is not a function");
-    }
-
-    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-    if (thisArg) {
-      T = thisArg;
-    }
-
-    // 6. Let A be a new array created as if by the expression new Array(len) where Array is
-    // the standard built-in constructor with that name and len is the value of len.
-    A = new Array(len);
-
-    // 7. Let k be 0
-    k = 0;
-
-    // 8. Repeat, while k < len
-    while(k < len) {
-
-      var kValue, mappedValue;
-
-      // a. Let Pk be ToString(k).
-      //   This is implicit for LHS operands of the in operator
-      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-      //   This step can be combined with c
-      // c. If kPresent is true, then
-      if (k in O) {
-
-        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-        kValue = O[ k ];
-
-        // ii. Let mappedValue be the result of calling the Call internal method of callback
-        // with T as the this value and argument list containing kValue, k, and O.
-        mappedValue = callback.call(T, kValue, k, O);
-
-        // iii. Call the DefineOwnProperty internal method of A with arguments
-        // Pk, Property Descriptor {Value: mappedValue, Writable: true, Enumerable: true, Configurable: true},
-        // and false.
-
-        // In browsers that support Object.defineProperty, use the following:
-        // Object.defineProperty(A, Pk, { value: mappedValue, writable: true, enumerable: true, configurable: true });
-
-        // For best browser support, use the following:
-        A[ k ] = mappedValue;
-      }
-      // d. Increase k by 1.
-      k++;
-    }
-
-    // 9. return A
-    return A;
-  };
-}
 (function() {
 
   var _config = {
     slide:{
-      duration:200
+      duration:1000
     }
   }
 
@@ -159,23 +86,55 @@ if (!Array.prototype.map) {
 
       var self = this;
       var duration = _config.slide.duration;
+      var first_animated = false;
 
       var marginLeft = parseInt(document.defaultView.getComputedStyle(this.$slide, null).marginLeft);
 
-      function frame(elem,position) {
-        marginLeft -= 50
+      function animate(opts) {
 
-        if (marginLeft <= position){
-          elem.style.marginLeft = position + 'px' // show frame
-          clearInterval(id)
-        } else {
-          elem.style.marginLeft = marginLeft + 'px' // show frame
-        }
+        var start = new Date
+
+        var id = setInterval(function() {
+          var timePassed = new Date - start
+          var progress = timePassed / opts.duration
+
+          if (progress > 1) progress = 1
+
+          var delta = opts.delta(progress)
+          opts.step(delta)
+
+          if (progress == 1) {
+            clearInterval(id)
+            var event = document.createEvent("HTMLEvents");
+            event.initEvent("slideEnd");
+            document.dispatchEvent(event);
+
+          }
+        }, opts.delay || 10)
+
       }
 
-      var id = setInterval(function(){
-        frame(self.$slide,position)
-      }, 1) // draw every 10ms
+      var delta = function quad(progress) {
+        return Math.pow(progress, 2)
+      }
+
+      var to = position;
+      animate({
+        delay: 10,
+        duration:  duration, // 1 sec by default
+        delta: delta,
+        step: function(delta) {
+          var position = (to - marginLeft) * delta;
+          self.$slide.style.marginLeft = marginLeft + position + "px"
+        }
+      })
+
+      document.addEventListener("slideEnd",function(){
+        if (!first_animated){
+          _config.slide.duration = 200;
+          first_animated = true;
+        }
+      });
 
       return ;
 
